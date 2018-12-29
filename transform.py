d265b214d19de5be5aa4d565813eb32eb170e9ef
fant <fantding@163.com>
2018-12-29 11:36:37 +0800

    1. done: 使用numpy矩阵运算代替循环，速度提升非常明显

--- transform.py
+++ transform.py
@@ -1,7 +1,6 @@
 import numpy as np
 import math
 import cv2
-from matplotlib import pyplot as plt
 
 
 def build_equ(four_corners):
@@ -11,15 +10,65 @@ def build_equ(four_corners):
     return X
 
 
-if __name__ == "__main__":
+def fast_bi_inter(src_img, height, width, T_x, T_y):
+    """
+    使用矩阵计算，实现快速双线性插值
+    :param src_img:
+    :param height:
+    :param width:
+    :return:
+    """
+    row_same = np.arange(0, height)
+    row_same = row_same[:, np.newaxis]
+    row_same = np.tile(row_same, (1, width))
+
+    col_same = np.arange(0, width)
+    col_same = col_same[np.newaxis, :]
+    col_same = np.tile(col_same, (height, 1))
+
+    mix_para = row_same * col_same
+
+    ones_para = np.ones(shape=(height, width))
+
+    paras = np.stack((row_same, col_same, mix_para, ones_para), axis=2)
+
+    paras = np.reshape(paras, (height * width, 4))
+
+    # 找到原图中的坐标
+    x = np.matmul(paras, T_x)
+    y = np.matmul(paras, T_y)
+    # 确定p1,p2,q1,q2
+    x_ceil = np.ceil(x).astype(int)
+    x_floor = np.floor(x).astype(int)
+
+    y_ceil = np.ceil(y).astype(int)
+    y_floor = np.floor(y).astype(int)
+
+    p1 = x - x_floor
+    # print(p1.shape) #(190512,)
+    p2 = 1 - p1
+    q1 = y - y_floor
+    q2 = 1 - q1
+
+    tar_img = np.expand_dims(p2 * q2, axis=1) * src_img[x_floor, y_floor, :] + \
+              np.expand_dims(p1 * q2, axis=1) * src_img[x_ceil, y_floor, :] + \
+              np.expand_dims(q1 * p1, axis=1) * src_img[x_ceil, y_ceil, :] + \
+              np.expand_dims(q1 * p2, axis=1) * src_img[x_floor, y_ceil, :]
+
+    tar_img=np.reshape(tar_img,(height,width,3))
+    return tar_img
+
+
+
+def main():
     # 图片角点顺序如下：
     # 0 1
     # 3 2
     # src_img_path="./data/000026.jpg"
     # detected_corner = [[87,96], [99,330], [459,339], [440,57]]
 
-    src_img_path="./data/000872.jpg"
-    detected_corner=[[119,74],[129,296],[442,306],[444,55]]
+    src_img_path = "./data/000872.jpg"
+    detected_corner = [[119, 74], [129, 296], [442, 306], [444, 55]]
 
     detected_corner = np.array(detected_corner, dtype=np.int32)
 
@@ -64,10 +113,34 @@ if __name__ == "__main__":
                                              src_img[x_ceil, y_ceil, channel] * q1 * p1 + \
                                              src_img[x_floor, y_ceil, channel] * q1 * p2
                 except:
-                    print(x_ceil,x_floor,y_ceil,y_floor)
+                    print(x_ceil, x_floor, y_ceil, y_floor)
                     print(src_img.shape)
-                    assert 1==9
+                    assert 1 == 9
     cv2.imshow('Window', np.uint8(tar_img))
     cv2.waitKey(0)
-    # plt.imshow(np.uint8(tar_img))
-    # plt.show()
+
+
+if __name__ == "__main__":
+    src_img_path = "./data/000872.jpg"
+    detected_corner = [[119, 74], [129, 296], [442, 306], [444, 55]]
+
+    detected_corner = np.array(detected_corner, dtype=np.int32)
+
+    height, width = [504, 378]
+    target_corner = np.array([[0, 0], [0, width - 1], [height - 1, width - 1], [height - 1, 0]], dtype=np.int32)
+
+    # 计算x,y变化矩阵T_x,T_y
+    leftMatrix = build_equ(target_corner)
+    inversed_leftMat = np.linalg.inv(leftMatrix)
+    X1 = detected_corner[:, 0]
+    T_x = np.matmul(inversed_leftMat, X1)
+
+    Y1 = detected_corner[:, 1]
+    T_y = np.matmul(inversed_leftMat, Y1)
+
+    src_img = cv2.imread(src_img_path)
+
+    tar_img=fast_bi_inter(src_img, height, width, T_x, T_y)
+    cv2.imshow('Window', np.uint8(tar_img))
+    cv2.waitKey(0)
+
