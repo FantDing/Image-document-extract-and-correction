89da032dcf68aaf61165990052d892445a171e9a
fant <fantding@163.com>
2019-01-03 20:12:16 +0800

    1. done: 清洁了明显接近的直线[@tag]

--- corner_detection.py
+++ corner_detection.py
@@ -27,7 +27,7 @@ def get_grad_img(gray_img):
 
 def hough_transform(gray_img):
     grad_img = get_grad_img(gray_img)
-    # theta_space = np.pi / 180
+    # --------------------------------------投票------------------------------------------
     rho_max = math.sqrt(grad_img.shape[0] * grad_img.shape[0] + grad_img.shape[1] * grad_img.shape[1])
     m, n = 180, 2000
     theta_range = np.linspace(0, np.pi, m)
@@ -41,6 +41,7 @@ def hough_transform(gray_img):
 
     cos_theta = np.cos(theta_range)
     sin_theta = np.sin(theta_range)
+    # 这是一个大坑，row实际对应的是y
     # theta_mat = np.stack((cos_theta, sin_theta), axis=0)  # 2*m
     theta_mat = np.stack((sin_theta, cos_theta), axis=0)  # 2*m
 
@@ -55,12 +56,30 @@ def hough_transform(gray_img):
     theta_ind = np.repeat(theta_ind, K)
 
     np.add.at(vote_table, (theta_ind, rho_ind), 1)  # 在vote_table中投票
+    # ----------------------------------过滤 1： 选出不同的直线-------------------------------
+    # 取出top_k条不同的直线
+    top_k = 5
+    argmax_ind = np.dstack(np.unravel_index(np.argsort(-vote_table.ravel(), ), (m, n)))
+    argmax_ind = argmax_ind[0, :, :]
+    valid_data = np.zeros((top_k, 2))
+    exist_num = 0
+    for i in range(0, m * n):
+        row_ind, col_ind = tuple(argmax_ind[i])
+        theta = theta_range[row_ind]
+        rho = rho_range[col_ind]
+        if is_new_line(theta, rho, valid_data, exist_num):
+            # 遇到新的线了
+            valid_data[exist_num][0] = theta
+            valid_data[exist_num][1] = rho
+            exist_num += 1
+            if exist_num >= top_k:
+                break
 
-    row_ind, col_ind = np.where(vote_table > 70)
-    for row, col in zip(row_ind, col_ind):
-        theta = theta_range[int(row)]
-        rho = rho_range[int(col)]
-        print(theta,rho)
+
+    #  绘制检测到的直线
+    for i in range(top_k):
+        theta, rho = tuple(valid_data[i])
+        print(theta, rho)
         a = np.cos(theta)
         b = np.sin(theta)
         x0 = a * rho
@@ -69,16 +88,18 @@ def hough_transform(gray_img):
         y1 = int(y0 + 1000 * (a))
         x2 = int(x0 - 1000 * (-b))
         y2 = int(y0 - 1000 * (a))
-        # cv2.line(grad_img, (10,10 ), (int(20), int(20)), (255,255,0), 3)
-        # cv2.line(grad_img,(x1,y1),(int(x0),int(y0)),(255,255,0), 3)
         cv2.line(grad_img, (x1, y1), (x2, y2), (255, 255, 0), 2)
 
-    # lines = cv2.HoughLinesP(grad_img, 1, np.pi / 180, threshold=40, minLineLength=80, maxLineGap=40)
-    # for i in range(int(np.size(lines) / 4)):
-    #     for x1, y1, x2, y2 in lines[i]:
-    #         cv2.line(grad_img, (x1, y1), (x2, y2), (255, 255, 0), 3)
-
     return grad_img
+
+
+def is_new_line(theta, rho, valid_data, exist_num):
+    for i in range(exist_num):
+        theta = 0 if theta - 3.1 > 0 else theta
+        if theta - valid_data[i][0] < 0.2 and np.square(np.abs(rho) - np.abs(valid_data[i][1])) < 1000:
+            # 角度相近 & rho相近
+            return False
+    return True
 
 
 def harries(gray):
@@ -94,8 +115,8 @@ def harries(gray):
 if __name__ == "__main__":
     path = "./data/000026.jpg"
     # path='./data/000872.jpg'
-    # path='./data/001201.jpg'
-    # path='./data/001402.jpg'
+    # path = './data/001201.jpg'
+    # path = './data/001402.jpg'
     # path = './data/001552.jpg'
     img = cv2.imread(path)
     gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
